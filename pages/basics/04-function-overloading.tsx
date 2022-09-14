7eb3769e6e72441911b1c7e0d5ce743ec08ba549
webdiego <webdiegomassarini@gmail.com>
2022-09-15 00:00:37 +0200

    feat(func-overloading)

--- pages/basics/04-function-overloading.tsx
+++ pages/basics/04-function-overloading.tsx
@@ -2,27 +2,109 @@ import Layout from '@components/Layout';
 import Code from '@components/Code';
 import Title from '@components/Title';
 
-
+interface Coordinate {
+  x: number;
+  y: number;
+}
 export default function FunctionOverloading() {
+  interface Coordinate {
+    x: number;
+    y: number;
+  }
+  function coordinateFromObject(obj: Coordinate): Coordinate {
+    return {
+      ...obj,
+    };
+  }
+  function coordinateFromNumbers(x: number, y: number): Coordinate {
+    return {
+      x,
+      y,
+    };
+  }
+  // function coordinate(obj: Coordinate): Coordinate;
+  // function coordinate(x: number, y: number): Coordinate;
+  function coordinate(arg1: unknown, arg2?: unknown): Coordinate {
+    let coord = {
+      x: 0,
+      y: 0,
+    };
+
+    if (typeof arg1 === 'object') {
+      coord = {
+        ...(arg1 as Coordinate),
+      };
+    } else {
+      coord = {
+        x: arg1 as number,
+        y: arg2 as number,
+      };
+    }
+    return coord;
+  }
+  //If the func takes one param (obj)
+  console.log(coordinate({ x: 10, y: 25 }));
+  //If the func takes two params
+  console.log(coordinate(10, 20));
   return (
     <Layout title="Function overloading">
       <div className="mt-10">
-        <Title text="Callbacks" />
-        <p>
-          We define the types inside a function, without say what should return, in this case is
-          inferring a type of number by default
-        </p>
+        <p>We define the types</p>
         <Code
-          code={`function printText(text: number, callback: () => void): void {
-  console.log(text);
-  callback();
-}`}
+          code={`interface Coordinate {
+    x: number;
+    y: number;
+  }`}
         />
+        <p>We define this function that take an object </p>
+        <Code
+          code={`function coordinateFromObject(obj: Coordinate): Coordinate {
+    return {
+      ...obj,
+    };
+  }`}
+        />
+        <p>Or we can also define this function that take an two params </p>
+        <Code
+          code={`function coordinateFromNumbers(x: number, y: number): Coordinate {
+    return {
+      x,
+      y,
+    };
+  }`}
+        />
+        <p>How can we abstract this function?</p>
+        <Code
+          code={`  function coordinate(obj: Coordinate): Coordinate;
+  function coordinate(x: number, y: number): Coordinate;
+  function coordinate(arg1: unknown, arg2?: unknown): Coordinate;`}
+        />
+        <p>Are the same function, are returning the same object</p>
+        <Code
+          code={`function coordinate(arg1: unknown, arg2?: unknown): Coordinate {
+    let coord = {
+      x: 0,
+      y: 0,
+    };
 
+    if (typeof arg1 === 'object') {
+      coord = {
+        ...(arg1 as Coordinate),
+      };
+    } else {
+      coord = {
+        x: arg1 as number,
+        y: arg2 as number,
+      };
+    }
+    return coord;
+  }
+  //If the func takes one param (obj)
+  console.log(coordinate( { x: 10 , y: 25 } ));
+  //If the func takes two params
+  console.log(coordinate( 10 , 20 ));`}
+        />
       </div>
-
-
-  
     </Layout>
   );
 }
