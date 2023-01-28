e1ce3be94bebc52169331f12be69f4113d31dbf4
webdiego <webdiegomassarini@gmail.com>
2023-01-28 13:12:07 +0100

    update(functions)

--- pages/basics/02-functions.tsx
+++ pages/basics/02-functions.tsx
@@ -2,22 +2,6 @@ import Layout from '@components/Layout';
 import Code from '@components/Code';
 import Title from '@components/Title';
 
-function product(a: number, b: number): number {
-  return a * b;
-}
-
-const addString = (str1: string, str2: string): string => `${str1} ${str2}`;
-const addString2 = (str1: string, str2: string = ''): string => `${str1} ${str2}`;
-const addString3 = (str1: string, str2: string | number): string => `${str1} ${str2}`;
-const addString4 = (title: string, param: string | number): void =>
-  console.log(`${title} ${param}`);
-
-function list(party: string, ...names: Array<string>): string {
-  return `${party} ${names}`;
-}
-
-const a = list('diego', 'h', 'sse');
-
 export default function Basic() {
   return (
     <Layout title="Functions">
@@ -39,19 +23,37 @@ export default function Basic() {
   return a * b;
 }`}
         />
-      </div>
-      {/* Function as a const */}
-      <div className="mt-10">
-        <Title text="Function as a const" />
+        <p>Other way is define the types outside the function</p>
         <Code
-          code={'const addString = (str1: string, str2: string): string => `${str1} ${str2}`'}
+          code={`type Product = (a: number, b: number) => number;
+
+let product: Product = (a, b) => {
+  return a * b;
+}; 
+
+// or
+
+let productInline: Product = (a, b) => a * b;
+`}
         />
       </div>
+
       {/* Default params */}
       <div className="mt-10">
         <Title text="Default params" />
         <Code
           code={"const addString2 = (str1: string, str2: string = ''): string => `${str1} ${str2}`"}
+        />
+        <Code
+          code={`const sumAll = (a: number = 2, b: number, c: number = 5): number => a + b + c;
+
+console.log(sumAll(2, 2, 4));
+console.log(sumAll(2, 2));
+console.log(sumAll(2)); // we get error because were not passing the second argument
+console.log(sumAll(undefined, 2)); 
+console.log(sumAll(undefined, 2, undefined));
+
+`}
         />
       </div>
       <div className="mt-10">
